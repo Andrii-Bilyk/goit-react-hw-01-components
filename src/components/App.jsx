22e524a3a2c99847cb0a3c295f97ea3ab39ec94b
Andrii Bilyk <bilykaim@gmail.com>
2023-10-25 11:16:40 +0300

    create all tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
+import React from 'react';
+import Profile from './Profile/Profile.jsx';
+import user from '../assets/user.json';
+
+import Statistics from './Statistics/Statistics.jsx';
+import data from '../assets/data.json';
+
+import FriendList from './FriendList/FriendList';
+import friends from '../assets/friends.json';
+
+import Transactions from './Transactions/Transactions';
+import transactions from '../assets/transactions.json';
+
 export const App = () => {
   return (
+    <div>
     <div
       style={{
         height: '100vh',
@@ -10,7 +24,27 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+
+    <div>
+      <Statistics title="Upload stats" stats={data} />
+    </div>
+
+    <div>
+      <FriendList friends={friends} />
+    </div>
+
+    <div>
+      <Transactions items={transactions} />
+    </div>
+
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+import styles from './FriendList.module.css';
+
+const FriendList = ({ friends }) => (
+  <ul className={styles['friend-list']}>
+    {friends.map((friend) => (
+      <FriendListItem key={friend.id} {...friend} />
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
